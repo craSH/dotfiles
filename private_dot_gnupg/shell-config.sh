3059040eb4b03f3ec873ad99892ae83db6eb0af0
Ian Gallagher <cdine@cdine.org>
2022-02-25 10:54:45 -0800

    Make gpg shell config a bit more robust (bash compatibility)

--- private_dot_gnupg/shell-config.sh
+++ private_dot_gnupg/shell-config.sh
@@ -1,4 +1,12 @@
+if [[ -z "$TTY" ]]; then
+  if ! TTY=$(tty >/dev/null 2>&1); then
+    echo "WARNING: Unable to determine current tty device - gpg-agent may fail (source: $0)" >&2
+  fi
+  export TTY
+fi
+
 export GPG_TTY=$TTY
+
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 gpgconf --launch gpg-agent
 gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1
